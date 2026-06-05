b5851a29c0f2939f89bb054747e046e42d5bbd77
mohitcusat <mohitchauhan7079@gmil.com>
2026-06-05 11:19:36 +0530

    Fix SMTP configuration

--- backend/controllers/sendMail.js
+++ backend/controllers/sendMail.js
@@ -2,14 +2,16 @@ const nodemailer = require("nodemailer");
 const sendMail = async (to, name, pdfPath) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
 
     await transporter.sendMail({
