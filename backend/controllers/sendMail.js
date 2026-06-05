24aa56fd61fe86a17a18cbe53dafd0b9c63c3ab7
mohitcusat <mohitchauhan7079@gmil.com>
2026-06-05 11:32:11 +0530

    Use Gmail service

--- backend/controllers/sendMail.js
+++ backend/controllers/sendMail.js
@@ -2,16 +2,17 @@ const nodemailer = require("nodemailer");
 const sendMail = async (to, name, pdfPath) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      //host: "smtp.gmail.com",
+      //port: 587,
+      //secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
+     // tls: {
+      //  rejectUnauthorized: false,
+     // },
     });
 
     await transporter.sendMail({
