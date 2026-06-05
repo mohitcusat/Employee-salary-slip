259fb70dc2af3f838e30e67493a16b4c846b18a3
mohitcusat <mohitchauhan7079@gmil.com>
2026-06-05 11:41:49 +0530

    gmail service fix

--- backend/controllers/sendMail.js
+++ backend/controllers/sendMail.js
@@ -2,17 +2,13 @@ const nodemailer = require("nodemailer");
 const sendMail = async (to, name, pdfPath) => {
   try {
     const transporter = nodemailer.createTransport({
-      //host: "smtp.gmail.com",
-      //port: 587,
-      //secure: false,
+    
       service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-     // tls: {
-      //  rejectUnauthorized: false,
-     // },
+     
     });
 
     await transporter.sendMail({
